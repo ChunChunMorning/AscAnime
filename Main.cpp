c2d5561775c4b5d95e8456993e0a76e9594b9cca
Asachun <chuchuchu.chunchun@gmail.com>
2016-09-22 16:11:29 +0900

    Add Sample.

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+﻿# include <Siv3D.hpp>
+# include "asc\Anime.hpp"
+
+void Main()
+{
+	asc::Anime anime(Texture(L"sample.png"), 4, 100);
+
+	anime.start();
+
+	while (System::Update())
+	{
+		anime.update();
+		anime.draw(Mouse::Pos());
+	}
+}
