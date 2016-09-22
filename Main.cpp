87c2964b87bf315cab8e06be05a270f8a931c0d3
Asachun <chuchuchu.chunchun@gmail.com>
2016-09-22 17:50:06 +0900

    Add Sample.

--- Main.cpp
+++ Main.cpp
@@ -3,10 +3,32 @@
 
 void Main()
 {
-	asc::Anime anime(Texture(L"sample.png"), 4, 100);
+	Texture texture(L"sample.png");
+	asc::Anime anime(texture, 4, 100);
 
 	while (System::Update())
 	{
 		anime.draw(Mouse::Pos());
 	}
 }
+
+
+/*
+
+// TextureAssetに登録されたTextureを使用する場合.
+
+# include <Siv3D.hpp>
+# include "asc\AnimeAsset.hpp"
+
+void Main()
+{
+	TextureAsset::Register(L"anime", L"sample.png");
+
+	asc::AnimeAsset anime(L"anime", 4, 100);
+
+	while (System::Update())
+	{
+		anime.draw(Mouse::Pos());
+	}
+}
+*/
